74c76402307c4d403b941af8ca29db2b2c2d1ca0
David Molinari <contact@davidmolinari.fr>
2016-04-15 16:47:28 +0200

    Binds Parsed items, fonctionnel

--- app/src/main/java/davidmolinari/rssreader/MyAdapter.java
+++ app/src/main/java/davidmolinari/rssreader/MyAdapter.java
@@ -5,6 +5,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -27,7 +31,10 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     @Override
     public void onBindViewHolder(MyAdapter.MyViewHolder holder, int position) {
-
+        FeedItem current = feedItems.get(position);
+        holder.Title.setText(current.getTitle());
+        holder.Description.setText(current.getDescription());
+        holder.PubDate.setText(current.getPubDate());
     }
 
 
@@ -37,8 +44,14 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView Title, Description, PubDate;
+        ImageView Image;
         public MyViewHolder(View itemView){
             super(itemView);
+            Title = (TextView) itemView.findViewById(R.id.textTitle);
+            Description = (TextView) itemView.findViewById(R.id.textDescription);
+            PubDate = (TextView) itemView.findViewById(R.id.textPubDate);
+            Image = (ImageView) itemView.findViewById(R.id.textImageLink);
 
         }
 
